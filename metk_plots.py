e6deaa467044ffe83f5272017957dabc64560bbf
Pat Walters <pwalters@PWALTERS-MBP00.local>
2017-07-27 15:34:55 -0400

    Initial commit

---
+++ metk_plots.py
@@ -0,0 +1,144 @@
+#!/usr/bin/env python
+
+import sys
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+from matplotlib.backends.backend_pdf import PdfPages
+import warnings
+from metk_util import kcal_to_ki_df, check_dataframe
+
+
+def ic50_histogram(df, ax, bins=None):
+    add_ic50_error(df)
+    if bins is None:
+        bins = [0, 5, 10, 15]
+    counts, hist_bins = np.histogram(np.clip(df['Error'], bins[0], bins[-1]), bins)
+    counts = counts / sum(counts)
+    ax.bar([1, 2, 3], counts, alpha=0.5, edgecolor="black")
+    ax.set_xticks([1, 2, 3])
+    ax.set_xticklabels(("<5", "5-10", ">10"))
+    ax.set_xlabel("Fold Error")
+    ax.set_ylabel("Normalized Count")
+
+
+def kcal_histogram(df, ax, bins=None):
+    add_kcal_error(df)
+    if bins is None:
+        bins = [0, 1, 2, 3]
+    counts, hist_bins = np.histogram(np.clip(df['Error'], bins[0], bins[-1]), bins)
+    counts = counts / sum(counts)
+    ax.bar([1, 2, 3], counts, alpha=0.5, edgecolor="black")
+    ax.set_xticks([1, 2, 3])
+    ax.set_xticklabels(("<1", "1-2", ">2"))
+    ax.set_xlabel("ΔG Error (kcal/mol)")
+    ax.set_ylabel("Normalized Count")
+
+
+def add_ic50_error(df, bins=None):
+    if bins is None:
+        bins = [5, 10]
+    pt_color = ['green', 'yellow', 'red']
+    df['Error'] = [10 ** x for x in np.abs(np.log10(df['Exp']) - np.log10(df['Pred']))]
+    df['Error_Bin'] = [pt_color[x] for x in np.digitize(df['Error'], bins)]
+
+
+def add_kcal_error(df, bins=None):
+    if bins is None:
+        bins = [1, 2]
+    pt_color = ['green', 'yellow', 'red']
+    df['Error'] = np.abs(df['Exp'] - df['Pred'])
+    df['Error_Bin'] = [pt_color[x] for x in np.digitize(df['Error'], bins)]
+
+
+def ic50_plot(df, ax, axis_range=None, units="uM"):
+    if axis_range is None:
+        axis_range = np.array([0.001, 100, 0.0001, 100])
+    if units == "nM":
+        axis_range *= 1000
+    min_x, max_x, min_y, max_y = axis_range
+    add_ic50_error(df)
+
+    ax.set(xscale="log", yscale="log")
+    ax.axis(axis_range)
+    ax.xaxis.set_major_formatter(
+        ticker.FuncFormatter(lambda y, pos: ('{{:.{:1d}f}}'.format(int(np.maximum(-np.log10(y), 0)))).format(y)))
+    ax.yaxis.set_major_formatter(
+        ticker.FuncFormatter(lambda y, pos: ('{{:.{:1d}f}}'.format(int(np.maximum(-np.log10(y), 0)))).format(y)))
+    ax.set_xlabel("Experimental IC50 (%s)" % units)
+    ax.set_ylabel("Predicted IC50 (%s)" % units)
+    ax.scatter(df['Exp'], df['Pred'], s=100, c=df['Error_Bin'], alpha=0.5, edgecolors="black")
+
+    ax.plot([0, max_x], [0, max_y], linewidth=2, color='black')
+    # 5 fold
+    ax.plot([0, max_x], [0, max_y * 5], linewidth=1, color="blue", linestyle='--')
+    ax.plot([0, max_x], [0, max_y / 5], linewidth=1, color="blue", linestyle='--')
+    # 10 fold
+    ax.plot([0, max_x], [0, max_y * 10], linewidth=1, color="black")
+    ax.plot([0, max_x], [0, max_y / 10], linewidth=1, color="black")
+
+
+def kcal_plot(df, ax, axis_range=None):
+    if axis_range is None:
+        axis_range = [-12, -6, -12, -6]
+    pt_color = ['green', 'yellow', 'red']
+    df['Error'] = np.abs(df['Exp'] - df['Pred'])
+    df['Error_Bin'] = [pt_color[x] for x in np.digitize(df['Error'], [1, 2])]
+    ax.axis(axis_range)
+    ax.set_xlabel("Experimental ΔG (kcal/mol)")
+    ax.set_ylabel("Predicted ΔG (kcal/mol)")
+    ax.scatter(df['Exp'], df['Pred'], s=100, c=df['Error_Bin'], alpha=0.5, edgecolors="black")
+    # y = x
+    ax.plot([0, -100], [0, -100], linewidth=2, color='black')
+    # y = x+1
+    ax.plot([-1, -100], [0, -100], linewidth=1, color="blue", linestyle='--')
+    # y = x-1
+    ax.plot([1, -100], [0, -100], linewidth=1, color="blue", linestyle='--')
+    # y = x+2
+    ax.plot([-2, -100], [0, -100], linewidth=1, color="black")
+    # y = x-2
+    ax.plot([2, -100], [0, -100], linewidth=1, color="black")
+
+
+def draw_plots(df_kcal, pdf_file_name, units='uM'):
+    add_kcal_error(df_kcal)
+    f_kcal, ax_kcal = plt.subplots(2, figsize=(7, 7))
+    ax_kcal[0].set_title("N = %d" % df_kcal.shape[0])
+    kcal_plot(df_kcal, ax_kcal[0])
+    kcal_histogram(df_kcal, ax_kcal[1])
+    pdf_pages = PdfPages(pdf_file_name)
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore")
+        plt.tight_layout()
+    pdf_pages.savefig(f_kcal.get_figure())
+
+    df_ic50 = kcal_to_ki_df(df_kcal, units)
+    add_ic50_error(df_ic50)
+    f_ic50, ax_ic50 = plt.subplots(2, figsize=(7, 7))
+    ic50_plot(df_ic50, ax_ic50[0], units=units)
+    ic50_histogram(df_ic50, ax_ic50[1])
+    pdf_pages.savefig(f_ic50.get_figure())
+
+    pdf_pages.close()
+
+
+def generate_pdf(figure_list, pdf_file_name):
+    pdf_pages = PdfPages(pdf_file_name)
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore")
+        plt.tight_layout()
+    for ax in figure_list:
+        pdf_pages.savefig(ax.get_figure())
+    pdf_pages.close()
+
+
+def main():
+    pdf_file_name = "/Users/pwalters/scratch/myplot.pdf"
+    df_kcal = pd.read_csv(sys.argv[1])
+    check_dataframe(df_kcal)
+    draw_plots(df_kcal, pdf_file_name, units="nM")
+
+
+if __name__ == "__main__":
+    main()
---
+++ metk_report.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+import sys
+from math import log10
+import pandas as pd
+from metk_util import rmse, mean_absolute_error, pearson_confidence, max_possible_correlation, kcal_to_ki
+from scipy.stats import pearsonr, kendalltau, spearmanr
+
+
+def metk_report(df_kcal, outfile=sys.stdout):
+    N = df_kcal.shape[0]
+    pred = df_kcal['Pred']
+    expr = df_kcal['Exp']
+    rms_val = rmse(pred, expr)
+    mae_val = mean_absolute_error(pred, expr)
+    pearson_r, pearson_p = pearsonr(pred, expr)
+    pearson_vals = [x ** 2 for x in [pearson_r] + list(pearson_confidence(pearson_r, N))]
+    spearman_r, spearman_p = spearmanr(pred, expr)
+    kendall_t, kendall_p = kendalltau(pred, expr)
+    max_correlation = max_possible_correlation([log10(kcal_to_ki(x)) for x in df_kcal['Exp']])
+    print("N = %d" % N, file=outfile)
+    print("RMSE = %.2f kcal/mol" % rms_val, file=outfile)
+    print("MAE  = %.2f kcal/mol" % mae_val, file=outfile)
+    print("Max possible correlation = %.2f" % max_correlation)
+    print("Pearson R^2 = %0.2f  95%%CI = %.2f %.2f" % tuple(pearson_vals), file=outfile)
+    print("Spearman rho = %0.2f" % spearman_r, file=outfile)
+    print("Kendall tau = %0.2f" % kendall_t, file=outfile)
+
+
+def main():
+    df_kcal = pd.read_csv(sys.argv[1])
+    metk_report(df_kcal)
+
+if __name__ == "__main__":
+    main()
